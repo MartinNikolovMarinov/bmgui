32f16b12dac129799e2c921da3c79a2d4ea416af
Martin Marinov <martin_n_marinov@protonmail.com>
2022-10-22 23:59:53 +0300

    opening a window with SFML

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
-int main(int argc, char const *argv[]) {
+#include <core.h>
+#include <fmt/core.h>
+#include <SFML/Window.hpp>
+
+using namespace coretypes;
+
+i32 main(i32, char const**) {
+    constexpr i32 N = 100;
+    sf::Window windows[N];
+    for (size_t i = 0; i < N; i++) {
+        windows[i].create(sf::VideoMode(800, 600), fmt::format("Window {}", i));
+    }
     return 0;
 }
